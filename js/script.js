56712c5c95cd2495c31d585cc970d4c748787b0c
kenkensigma <balidenpasarbali123@gmail.com>
2024-11-22 08:15:37 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 100);
 });
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.navlist');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
