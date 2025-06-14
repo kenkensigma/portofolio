932fa83ffdc765dd59c6f2ad2b394ebc713ae95e
Raditya Putra <balidenpasarbali123@gmail.com>
2025-06-14 17:22:06 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,3 +9,36 @@ const navList = document.querySelector('.navlist');
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
+
+  window.addEventListener("load", () => {
+    document.querySelectorAll(".progress").forEach((progressEl) => {
+      const target = parseInt(progressEl.dataset.value);
+      let current = 0;
+
+      const interval = setInterval(() => {
+        if (current >= target) {
+          clearInterval(interval);
+        } else {
+          current++;
+          progressEl.style.width = current + "%";
+          progressEl.querySelector(".progress-text").textContent = current + "%";
+        }
+      }, 20); // Semakin kecil semakin cepat (20ms antar step)
+    });
+  });
+
+const reveals = document.querySelectorAll('.reveal');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  reveals.forEach(section => {
+    observer.observe(section);
+  });
